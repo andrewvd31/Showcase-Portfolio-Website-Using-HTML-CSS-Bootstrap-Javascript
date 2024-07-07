4f46e93d1d730254bee88ec3a916a8ebd29e5e86
Andrew V D <andrewvd31@gmail.com>
2024-07-07 15:02:07 +0530

    Update v1.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,18 @@
-//imp! Navbar Active Script
+// imp! Navbar Active Script
+
 let sections = document.querySelectorAll("section");
+
 let navLinks = document.querySelectorAll(".nav-link");
 
 window.onscroll = () => {
   sections.forEach((sec) => {
-    if (sec.id === 'hero-section' || sec.id === 'about-section' || sec.id === 'services-section' || sec.id === 'portfolio' || sec.id === 'contact-us'){
+    if (
+      sec.id === "hero-section" ||
+      sec.id === "about-section" ||
+      sec.id === "services-section" ||
+      sec.id === "portfolio" ||
+      sec.id === "contact-us"
+    ) {
       let top = window.scrollY;
       let offset = sec.offsetTop;
       let height = sec.offsetHeight;
@@ -13,7 +21,7 @@ window.onscroll = () => {
         navLinks.forEach((links) => {
           links.classList.remove("active");
           document
-            .querySelector(`.nav-link[href="#${sectionId}"]`)
+            .querySelector(`.nav-link[href="#${sectionId}"`)
             .classList.add("active");
         });
       }
@@ -21,19 +29,22 @@ window.onscroll = () => {
   });
 };
 
-//imp! Email-JS Script
-document.getElementById("contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  let params = {
-    from_name: document.getElementById("full_name").value,
-    email_id: document.getElementById("email_id").value,
-    message: document.getElementById("message").value,
-  };
-  console.log(params.from_name);
-  emailjs.send("service_wo4n3o8", "template_qewt3hq", params).then(() => {
-    document.querySelector(".thank-you-msg").style.display = "block";
-    setTimeout(() => {
-      document.querySelector(".thank-you-msg").style.display = "none";
-    }, 2000);
-  });
-});
+//imp! Email JS Script
+
+document.getElementById('contact-form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let params = {
+        from_name: document.getElementById('full_name').value,
+        email_id: document.getElementById('email_id').value,
+        message: document.getElementById('message').value
+    }
+    emailjs.send("service_59chgtn","template_8z6zpql",params).then(()=>{
+        document.querySelector('.thank-you-msg').style.display = "block";
+        document.getElementById('full_name').value = ""
+        document.getElementById('email_id').value = ""
+        document.getElementById('message').value = ""
+        setTimeout(()=>{
+            document.querySelector('.thank-you-msg').style.display = "none";
+        },2500)
+    })
+})
